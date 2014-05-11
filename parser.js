d1d8685c381a2f89a41f1319b4015402baf2ae6a
Avenger7x <avenger7x13@gmail.com>
2014-05-11 17:25:16 +0300

    browser compatibility

---
+++ parser.js
@@ -0,0 +1,30 @@
+!function(module, global){
+var exports = module.exports;
+/* jshint browser:true */
+'use strict';
+
+var parser = new DOMParser();
+
+function parseShim(html){
+	var clean = document.implementation.createHTMLDocument('');
+	clean.body.innerHTML = html;
+	return clean;
+}
+
+function parse(html){
+	return parser.parseFromString(html, 'text/html') || parseShim(html);
+}
+
+module.exports = function(html){
+	var fragment = document.createDocumentFragment(),
+		body = parse(html).body;
+
+	while(body.firstChild){
+		fragment.appendChild(body.firstChild);
+	}
+
+	return fragment;
+};
+
+global.parse = module.exports;
+}({ exports: {} }, function(){ return this; }());
