05bb2dd2c5c0cbeb7de6a372969ee04344f6c258
Avenger7x <avenger7x13@gmail.com>
2014-05-11 16:10:44 +0300

    shim for safari

--- index.js
+++ index.js
@@ -2,11 +2,24 @@
 
 var parser = new DOMParser();
 
+function parseShim(html){
+	var doc = document.implementation.createHTMLDocument('');
+
+	doc.body.innerHTML = html;
+
+	return doc;
+}
+
+function parse(html){
+	return parser.parseFromString(html, 'text/html') || parseShim(html);
+}
+
 module.exports = function(html){
 	var fragment = document.createDocumentFragment(),
-		doc = parser.parseFromString(html, 'text/html');
+		doc = parse(html);
 
-	[].slice.call(doc.body.childNodes).forEach(fragment.appendChild, fragment);
+	[].slice.call(doc.body.childNodes)
+		.forEach(fragment.appendChild, fragment);
 
 	return fragment;
 };
