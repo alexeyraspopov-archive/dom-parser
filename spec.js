6260b1e058f13c1a03a0f5f4a80e4c88dae53008
Avenger7x <avenger7x13@gmail.com>
2014-05-11 17:24:01 +0300

    specs

--- spec.js
+++ spec.js
@@ -1,8 +1,15 @@
-/* global describe, it, beforeEach, expect, document */
+/* global describe, it, expect, document */
 'use strict';
 
-var parser = require('./index');
+var parse = require('./index');
 
 describe('parser', function(){
+	it('should parse', function(){
+		var html = '<div id="1"> <div id="2"> <div id="3"></div> <div id="5"></div> </div> <div id="6"></div> </div>',
+			stub = document.createElement('div');
+
+		stub.appendChild(parse(html));
+		expect(stub.innerHTML).toBe(html);
+	});
 
 });
